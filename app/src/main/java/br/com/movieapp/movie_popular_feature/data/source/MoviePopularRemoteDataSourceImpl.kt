871b2b5ba6adab24bf20861b269fac953bd1cbaa
Gabriel Morais <gmdeveloper@gmail.com>
2023-12-05 21:33:24 -0300

    até a aula 23 * Criação do MovieItem * Implementação do GetPopularMovieUseCase, MoviePopularFeatureModule, MoviePopularRemoteDataSourceImpl

--- app/src/main/java/br/com/movieapp/movie_popular_feature/data/source/MoviePopularRemoteDataSourceImpl.kt
+++ app/src/main/java/br/com/movieapp/movie_popular_feature/data/source/MoviePopularRemoteDataSourceImpl.kt
@@ -3,8 +3,9 @@ package br.com.movieapp.movie_popular_feature.data.source
 import br.com.movieapp.core.data.remote.MovieService
 import br.com.movieapp.core.data.remote.response.MovieResponse
 import br.com.movieapp.movie_popular_feature.domain.source.MoviePopularRemoteDataSource
+import javax.inject.Inject
 
-class MoviePopularRemoteDataSourceImpl(
+class MoviePopularRemoteDataSourceImpl @Inject constructor(
   private val service: MovieService
 ) : MoviePopularRemoteDataSource {
   override suspend fun getPopularmovies(page: Int): MovieResponse {
